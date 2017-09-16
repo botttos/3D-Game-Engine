6926322f7d1b40670c883595585bae1650f4c45a
AlexisCosano <alcoro13@gmail.com>
2017-09-16 17:11:52 +0200

    Main menu bar
    - Added the main menu bar.
    - Added some placeholder text on the main menu bar.
    - Added the window tab where you can open and close the Test Window.

--- 3D Engine/ModuleSceneIntro.cpp
+++ 3D Engine/ModuleSceneIntro.cpp
@@ -52,8 +52,84 @@ update_status ModuleSceneIntro::Update(float dt)
 	Plane p(0, 1, 0, 0);
 	p.axis = true;
 	p.Render();
+
+	static bool show_test_window = false;
+
+	if (show_test_window)
+	{
+		ImGui::ShowTestWindow();
+	}
+
+	if (ImGui::BeginMainMenuBar())
+	{
+		if (ImGui::BeginMenu("File"))
+		{
+			ImGui::Text("New file");
+			ImGui::Text("Open file");
+			ImGui::Separator();
+			ImGui::Text("Save file");
+			ImGui::EndMenu();
+		}
+
+		if (ImGui::BeginMenu("Edit"))
+		{
+			ImGui::Text("Undo");
+			ImGui::Text("Redo");
+			ImGui::Separator();
+			ImGui::Text("Cut");
+			ImGui::Text("Copy");
+			ImGui::Text("Paste");
+			ImGui::Separator();
+			ImGui::Text("Duplicate");
+			ImGui::Text("Delete");
+			ImGui::EndMenu();
+		}
+
+		if (ImGui::BeginMenu("Assets"))
+		{
+			ImGui::Text("Import new asset");
+			ImGui::EndMenu();
+		}
+
+		if (ImGui::BeginMenu("GameObject"))
+		{
+			ImGui::Text("Create empty");
+			if (ImGui::BeginMenu("3D object"))
+			{
+				ImGui::Text("Cube");
+				ImGui::Text("Sphere");
+				ImGui::Text("Capsule");
+				ImGui::Text("Cylinder");
+				ImGui::Separator();
+				ImGui::Text("Plane");
+				ImGui::Text("Quad");
+				ImGui::EndMenu();
+			}
+			ImGui::EndMenu();
+		}
+
+		if (ImGui::BeginMenu("Component"))
+		{
+			ImGui::Text("Mesh");
+			ImGui::Text("Physics");
+			ImGui::EndMenu();
+		}
+
+		if (ImGui::BeginMenu("Window"))
+		{
+			ImGui::Checkbox("Show test window", &show_test_window);
+			ImGui::EndMenu();
+		}
+
+		if (ImGui::BeginMenu("About"))
+		{
+
+			ImGui::EndMenu();
+		}
+		
+		ImGui::EndMainMenuBar();
+	}
 	
-	ImGui::ShowTestWindow();
 	ImGui::Render();
 
 	return UPDATE_CONTINUE;
