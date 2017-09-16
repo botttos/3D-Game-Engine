1b77ebd0ebeae038677aca5b8e54e22d7b57f6b7
Fran <fran152job@gmail.com>
2017-09-16 20:10:41 +0200

    Polished grid

--- 3D Engine/ModulePhysics3D.cpp
+++ 3D Engine/ModulePhysics3D.cpp
@@ -117,7 +117,7 @@ update_status ModulePhysics3D::Update(float dt)
 
 	if(debug == true)
 	{
-		world->debugDrawWorld();
+		//world->debugDrawWorld();
 
 		// Render vehicles
 		p2List_item<PhysVehicle3D*>* item = vehicles.getFirst();
