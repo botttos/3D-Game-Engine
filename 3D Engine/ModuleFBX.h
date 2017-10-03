3d901d13ddf8280de4ff5dca37008953a1544348
AlexisCosano <alcoro13@gmail.com>
2017-10-03 12:37:35 +0200

    Struct ModelConfig

--- 3D Engine/ModuleFBX.h
+++ 3D Engine/ModuleFBX.h
@@ -2,6 +2,18 @@
 
 #include "Module.h"
 
+struct ModelConfig
+{
+private:
+	uint id_vertices = 0;
+	uint num_indices = 0;
+	uint* indices = nullptr;
+
+	uint id_indices = 0;
+	uint num_vertices = 0;
+	float* vertices = nullptr;
+};
+
 class ModuleFBX : public Module
 {
 public:
